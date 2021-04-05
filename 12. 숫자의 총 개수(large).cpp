37554cd4c6da09a5785ea0c65b79ad2b06bab1ec
rudy0103 <qs2720@gmail.com>
2021-04-05 18:00:23 +0900

    modified

--- 12. 숫자의 총 개수(large).cpp
+++ 12. 숫자의 총 개수(large).cpp
@@ -5,8 +5,8 @@
 int main(void){
     int n,i, cnt,sum,total=0 ;
 
-    // scanf("%d",&n);
-    n = 100000000;
+    scanf("%d",&n);
+    // n = 100000000;
     cnt = 10;
 
     for(i = 1000000000; i >= 1 ; i = i/10){
@@ -22,7 +22,7 @@ int main(void){
     cnt--;
 
     for (int j = cnt; j >=1 ; j--){
-        sum = ((pow(10,j) - pow(10,j-1)))*j;
+        sum = ((round(pow(10,j)) - round(pow(10,j-1))))*j;
         total= total + sum;
 
     }
